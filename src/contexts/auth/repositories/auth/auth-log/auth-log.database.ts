592c93adb032cf2b5fe1290fef5fa2f595339620
Pablo amomo <pabloamomo@gmail.com>
2024-03-15 10:12:46 +0100

    .

--- src/contexts/auth/repositories/auth/auth-log/auth-log.database.ts
+++ src/contexts/auth/repositories/auth/auth-log/auth-log.database.ts
@@ -1,15 +1,16 @@
 import getMySqlDatabaseConfig from '@functions/get-mysql-database-config.function';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import {
-  REPO_DATASOURCE,
-  REPO_DATASOURCE_LOG,
-  REPO_DB,
-} from '../auth.constants';
+import { REPO_DATASOURCE_LOG, REPO_DB } from '../auth.constants';
 
 export const DatabaseAuthLog = {
   name: REPO_DATASOURCE_LOG,
   imports: [ConfigModule],
   useFactory: async (configService: ConfigService) =>
-    getMySqlDatabaseConfig(__dirname, REPO_DATASOURCE, REPO_DB, configService),
+    getMySqlDatabaseConfig(
+      __dirname,
+      REPO_DATASOURCE_LOG,
+      REPO_DB,
+      configService,
+    ),
   inject: [ConfigService],
 };
--- src/contexts/user/repositories/user-log/user-log.database.ts
+++ src/contexts/user/repositories/user-log/user-log.database.ts
@@ -1,15 +1,16 @@
 import getMySqlDatabaseConfig from '@functions/get-mysql-database-config.function';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import {
-  REPO_DATASOURCE,
-  REPO_DATASOURCE_LOG,
-  REPO_DB,
-} from '../user.constants';
+import { REPO_DATASOURCE_LOG, REPO_DB } from '../user.constants';
 
 export const DatabaseUserLog = {
   name: REPO_DATASOURCE_LOG,
   imports: [ConfigModule],
   useFactory: async (configService: ConfigService) =>
-    getMySqlDatabaseConfig(__dirname, REPO_DATASOURCE, REPO_DB, configService),
+    getMySqlDatabaseConfig(
+      __dirname,
+      REPO_DATASOURCE_LOG,
+      REPO_DB,
+      configService,
+    ),
   inject: [ConfigService],
 };
